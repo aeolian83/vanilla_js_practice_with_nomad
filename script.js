f7eb1484f93798a5b0aa2818a8aaf1e88ea84efe
aeolian83 <aeolian83@gmail.com>
2022-01-01 11:07:48 +0900

    #2.3 const and let (09:45)

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const a = 5;
-const b = 2;
+let a = 5;
+var b = 2;
 
-const myName = "Jungki";
+let myName = "Jungki";
 const veryLongVarianbleName = 0;
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("Hello " + myName);
+
+myName = "Son"
+
+console.log("Your new name is " + myName);
