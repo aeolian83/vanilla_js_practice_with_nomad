4f95bc8d28dceb0a819ce1dac6aef73bc53fb5c8
aeolian83 <aeolian83@gmail.com>
2022-01-01 10:53:49 +0900

    #2.2 Variables (10:39)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hi");
+const a = 5;
+const b = 2;
+
+const myName = "Jungki";
+const veryLongVarianbleName = 0;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello " + myName);
