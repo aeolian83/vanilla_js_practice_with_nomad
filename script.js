54b00978bef5fa35d29121b5dd19bcd7b5256529
aeolian83 <aeolian83@gmail.com>
2022-01-04 07:34:10 +0900

    #2.8 Functions part Two (12:45)

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function sayHello(){
-    console.log("Hello my name is ");
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
 
 console.log("Hello!");
-sayHello("nico")
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello('lynn', 21);
+
+function plus(a, b){
+    console.log(a + b);
+}
+
+plus();
+plus(8, 60)
+
+function divide(a, b){
+    console.log(a / b);
+}
+
+divide(60, 8);
+
+
+const player = {
+    name: "Jungki",
+    sayHello: function (otherPersonsName){
+        console.log("Hello!!! " + otherPersonsName + " Nice to meet you");
+    },
+}
+
+player.sayHello();
+
+player.sayHello("Lynn");
