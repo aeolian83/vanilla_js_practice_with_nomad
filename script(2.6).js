fcaf91329d52c797fba98cde32a0285f1a8c9212
aeolian83 <aeolian83@gmail.com>
2022-01-04 06:59:34 +0900

    #2.7 Functions part One (08:44)

---
+++ script(2.6).js
@@ -0,0 +1,30 @@
+const playerName = "Jungki";
+const playerPoints = 2232323;
+const playerHansome = null;
+const playerFat = "Little Bit"
+
+
+const player1 = ["Jungki", 121212, false, "Little Bit"]
+
+const player = {
+    name: "Jungki",
+    points: 20,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+player.fat = false
+
+console.log(player);
+console.log(player.fat);
+
+player.lastName = "Son"
+
+console.log(player);
+
+player.points = player.points + 15;
+
+console.log(player.points);
+console.log(player)
--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-const playerName = "Jungki";
-const playerPoints = 2232323;
-const playerHansome = null;
-const playerFat = "Little Bit"
+function sayHello(){
+    console.log("Hello!");
+}
 
-
-const player1 = ["Jungki", 121212, false, "Little Bit"]
-
-const player = {
-    name: "Jungki",
-    points: 20,
-    fat: true,
-};
-
-console.log(player);
-console.log(player.name);
-
-player.fat = false
-
-console.log(player);
-console.log(player.fat);
-
-player.lastName = "Son"
-
-console.log(player);
-
-player.points = player.points + 15;
-
-console.log(player.points);
-console.log(player)
+sayHello()
+sayHello()
+sayHello()
+sayHello()
