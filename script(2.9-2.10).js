9a6b1a5f65b5ecb7597201606c7e7cf59c9a4cf3
aeolian83 <aeolian83@gmail.com>
2022-01-05 08:08:32 +0900

    #2.11 Returns (15:43)

---
+++ script(2.9-2.10).js
@@ -0,0 +1,23 @@
+calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    multi: function (a, b) {
+        console.log(a * b);
+    },
+    power: function (a, b) {
+        console.log(a**b);
+    },
+}
+
+calculator.add(1, 5);
+calculator.minus(1, 5);
+calculator.divide(6, 3);
+calculator.multi(3, 7);
+calculator.power(2, 5);
