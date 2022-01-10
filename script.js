94cd5b3b90c1422d791d415a17f1ccb63ce78a46
aeolian83 <aeolian83@gmail.com>
2022-01-11 08:59:58 +0900

    #3.7 CSS in Javascript part Two (09:34)

--- script.js
+++ script.js
@@ -2,33 +2,25 @@ const h1 = document.querySelector(".hello h1");
 
 console.dir(h1);
 
-//function handTitleClick(){
+// function handTitleClick(){
 //    if(h1.style.color === "blue") {
 //        h1.style.color = "tomato";
 //    } else {
 //        h1.style.color = "blue";
 //    }
-//}
+// }
 
 // function handTitleClick(){
-//     const currentColor = h1.style.color;
-//     let newColor;
-//     if(currentColor === "blue") {
-//         newColor = "tomato";
-//     } else {
-//         newColor = "blue";
-//     }
-//     h1.style.color = newColor;
+//     h1.className = "active";
 // }
 
 function handTitleClick(){
-    const currentColor = h1.style.color;
-    if(currentColor === "blue") {
-        currentColor = "tomato";
+    const clickedClass = "clicked"
+    if(h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        currentColor = "blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = currentColor;
 }
 
 h1.addEventListener("click", handTitleClick);
