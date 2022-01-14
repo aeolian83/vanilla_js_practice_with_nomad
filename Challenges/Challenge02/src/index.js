ac94aa8759f9ddb07a7efc6aadf764b49f333454
aeolian83 <aeolian83@gmail.com>
2022-01-14 23:09:01 +0900

    Challenge02 enhacing

--- Challenges/Challenge02/src/index.js
+++ Challenges/Challenge02/src/index.js
@@ -1,14 +1,24 @@
 const body = document.querySelector("body");
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
 
 function resizeBackground() {
   const bodyWidth = window.innerWidth;
   if (bodyWidth < 500) {
     body.style.backgroundColor = "#2e8cd5";
-  } else if (bodyWidth >= 500 && bodyWidth < 900) {
+  } else if (bodyWidth >= 500 && bodyWidth <= 900) {
     body.style.backgroundColor = "#904fad";
   } else if (bodyWidth > 900) {
     body.style.backgroundColor = "#eebc12";
   }
 }
 
-window.onresize = resizeBackground;
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+// window.onresize = resizeBackground;
+
+window.addEventListener("resize", resizeBackground);
+window.addEventListener("resize", reportWindowSize);
