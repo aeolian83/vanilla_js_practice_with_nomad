a6f11c4b4a883151046c9cf0f994c9ce891d0211
aeolian83 <aeolian83@gmail.com>
2022-01-18 04:50:43 +0900

    #4.5-4.6 Save & Loading Username (10:07)

--- script(4.4).js
+++ script(4.4).js
@@ -5,7 +5,6 @@ const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
 
 // function onLoginSubmit(event) {
 //     event.preventDefault();
@@ -20,24 +19,8 @@ function onLoginSubmit(event) {
   loginform.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   // greeting.innerText = "Hello " + username;
-  localStorage.setItem(USERNAME_KEY, username);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
-
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if (savedUsername === null) {
-  // show the form
-  loginform.classList.remove(HIDDEN_CLASSNAME);
-  loginform.addEventListener("submit", onLoginSubmit);
-} else {
-  // show the greeting
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${savedUsername}`;
-}
+loginform.addEventListener("submit", onLoginSubmit);
