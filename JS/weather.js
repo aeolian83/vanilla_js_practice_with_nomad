ebe9db4b6588260248a338ac6b3dbe50ca608ab7
aeolian83 <aeolian83@gmail.com>
2022-01-24 00:50:41 +0900

    #8.1 Weather API (14:13)

--- JS/weather.js
+++ JS/weather.js
@@ -1,5 +1,18 @@
+
+const API_KEY = "b3c97226ca0f0cb931d686e54ce8656f"
+
 function onGeoOk(position) {
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&unit=metric`;
+    fetch(url)
+    .then(respones => respones.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
