70282c8921b427b259c8a991811728ecf776be48
aeolian83 <aeolian83@gmail.com>
2022-01-24 00:11:41 +0900

    #7.6-8.0

--- JS/todo.js
+++ JS/todo.js
@@ -14,12 +14,15 @@ function saveToDos() {
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    span.innerText = newToDo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -32,9 +35,13 @@ function paintToDo(newToDo) {
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
-    toDoInput.value = ""
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    toDoInput.value = "";
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now(),
+    };
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
@@ -46,7 +53,7 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if(saveToDos !== null) {
+if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
     // parsedToDos.forEach(sayHello);
---
+++ JS/weather.js
@@ -0,0 +1,10 @@
+function onGeoOk(position) {
+    console.log(position);
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
